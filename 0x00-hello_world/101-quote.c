9dcdf354f379898913e12673e74b4c3a3dc76d23
johnenes <johneneojo1@gmail.com>
2023-06-15 12:49:30 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 * main - A c pogram that print a line to standard error
 * Return 1 (success)
 */
-int main(){
+int main(void){
 	write(2,"and that piece of art is useful\"  - Dora Korpar, 2015-10-19\n",58);
 	return (1); 
 }
