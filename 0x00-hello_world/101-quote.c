79554825c68e05afcf146f420b231c5c7e29cb4b
johnenes <johneneojo1@gmail.com>
2023-06-15 12:37:28 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/** 
+* main - A c pogram that print a line to standard error
+* Return 1 (success)
+*/
+int main(){
+fputs("and that piece of art is useful\"  - Dora Korpar, 2015-10-19\r", stdout);
+return (1); 
+}
