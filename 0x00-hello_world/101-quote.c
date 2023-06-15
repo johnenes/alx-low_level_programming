116f0899ff40a496951579183d5f0099ce48f6a5
johnenes <johneneojo1@gmail.com>
2023-06-15 13:52:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
-/** 
-*main - A c pogram that print a line to standard error
-*Return: 1 (Failure)
-*/
+
+/**
+ * main - print quote
+ *
+ * Description: prints Dors's quote
+ *
+ * Return: Always 1 (Success)
+ */
+
 int main(void)
 {
-write(2,"and that piece of art is useful\"  - Dora Korpar, 2015-10-19\n", 58);
-return (1); 
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
