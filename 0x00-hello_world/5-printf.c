d3c5fe6b58c1d1e36ece17f6f40feee5ef45bdf7
johnenes <johneneojo1@gmail.com>
2023-06-15 08:03:03 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
 *main - A program that print exactly using printf function
 *Return: 0 (Success)
 */
-int main(void){
-printf ("with proper grammar, but the outcome is a piece of art," );
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art," );
 return(0);
 }
