4000a318ef6d2e054762f0ec1a475c63ee9fd9d3
johnenes <johneneojo1@gmail.com>
2023-06-15 07:48:53 -0700

    deted file

--- 0x00-hello_world/5-printf.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-/**
-*main - A program that print exactly using printf function
-*Return: 0 (Success)
-*/
-int main (void){
-printf ( " with proper grammar, but the outcome is a piece of art," );
-return(0);
-}
