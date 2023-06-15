b7de6e42be59656c8342e6b1134d084b7be75df8
johnenes <johneneojo1@gmail.com>
2023-06-15 08:05:37 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
-*main - A program that print exactly using printf function
-*Return: 0 (Success)
+* main - A program that print exactly using printf function
+* Return: 0 (Success)
 */
 int main(void)
 {
