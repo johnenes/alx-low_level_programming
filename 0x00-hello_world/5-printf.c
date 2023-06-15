80e6ff1f5508f0728c322c350325dd869e1b4d3f
johnenes <johneneojo1@gmail.com>
2023-06-15 07:34:24 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include<stdion.h>
+/**
+*main - A program that print exactly using printf function
+*Return: 0 (Success)
+*/
+int main (){
+printf("%c","with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
