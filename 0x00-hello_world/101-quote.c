8a252a015d95f1a52a5b3f04807d16e2de26925f
johnenes <johneneojo1@gmail.com>
2023-06-15 12:53:51 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 #include <unistd.h>
 /** 
 *main - A c pogram that print a line to standard error
-*Return 1 (success)
+*Return: 1 (Failure)
 */
 int main(void){
-	write(2,"and that piece of art is useful\"  - Dora Korpar, 2015-10-19\n",58);
+	write(2,"and that piece of art is useful\"  - Dora Korpar, 2015-10-19\n", 58);
 	return (1); 
 }
