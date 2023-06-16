d94f70d3b52e7e2a884e51c7c671211a8d39368f
johnenes <johneneojo1@gmail.com>
2023-06-16 12:59:16 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,13 @@
  * Description: a program that prints the alphabet in lowercase using the putchar 
  * Return: 0
  */
-int main()
+int main(void)
 {	
 	char letter;
 
 	for(letter = 'a';letter <='z'; letter++)
 		putchar(letter);
-
+		putchar("\n");
 
 	return (0);
 }
