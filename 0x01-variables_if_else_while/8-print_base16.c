ff4b7f0502eca2d1a70d650459dfba58419cfee0
johnenes <johneneojo1@gmail.com>
2023-06-16 16:19:14 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - Entry Point
+ * Description :  program that prints all the numbers of base 16 in lowercase 
+ * return 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
