476c61c6c139bb4397d1d65f9734d9024b7ecabc
johnenes <johneneojo1@gmail.com>
2023-06-16 14:53:43 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: prints all the single-digit numbers of base from 0
+ * Return Alway Success
+ */
+int main(void) {
+	int i;
+
+	for(i=0;  i <= 10 ; i++){
+		printf("%d" , i);
+	}
+
+	return 0;
+}
+
