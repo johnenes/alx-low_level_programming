08d6ed5d1240f85c37b6ebedd159ca943e1e6ab4
johnenes <johneneojo1@gmail.com>
2023-06-16 15:03:36 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,12 +4,13 @@
  * Description: prints all the single-digit numbers of base from 0
  * Return: Alway Success
  */
-int main(void) {
+int main(void)
+{
 	
 	int i;
 
 
-	for(i=0;  i <= 9 ; i++)
+	for(i = 0;  i <= 9 ; i++)
 		printf("%d" , i);
 	
 
