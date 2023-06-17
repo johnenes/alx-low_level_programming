29c33f8e0947b4a5f1acb7c70156838b894d7e26
johnenes <johneneojo1@gmail.com>
2023-06-17 11:20:39 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 /**
  *main - Entry point
- *Description: a program that prints the alphabet 
+ *Description: A program that prints the alphabet 
  *in lowercase using the putchar 
  *Return: 0
  */
 
 int main(void)
 {
-	
 int i;
 
 for (i = 0; i < 26; i++)
