7aea97517e270942846f9a17f3d83a057196ac22
johnenes <johneneojo1@gmail.com>
2023-06-17 12:55:50 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,14 +3,14 @@
  * main - program that prints the lowercase alphabet in reverse
  * Return: 0 (Success)
  */
-
-int main(void)
-{
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		putchar(i + '0');
-	}
-	return (0);
+int main() {
+    int num = 0;
+    
+    while (num <= 9) {
+        putchar('0' + num);
+        putchar('\n');
+        num++;
+    }
+    
+    return 0;
 }
