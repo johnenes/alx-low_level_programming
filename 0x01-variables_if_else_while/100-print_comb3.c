536b0dbe13eeddfad0bed6f092160c70b92308b9
johnenes <johneneojo1@gmail.com>
2023-06-16 21:42:24 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,10 +19,10 @@ int  main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for(j = i +1 ; j < 10; j++)
+		for (j = i +1 ; j < 10; j++)
 		{
-			putchar( i + '0');
-			putchar( j + '0');
+			putchar ( i + '0');
+			putchar ( j + '0');
 			if (i != 8 || j != 9)
 			{
 				putchar (',');
