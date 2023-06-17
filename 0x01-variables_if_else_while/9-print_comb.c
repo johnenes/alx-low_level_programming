3722e05ddfe61b9164d468bb3a5684f72b6acbf2
johnenes <johneneojo1@gmail.com>
2023-06-16 21:08:19 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 /**
  * main - Entry Point
- * Description:The program print all possible
+ * Description: The program print all possible
  * combination of a single digit numbers.
  * the number must be seperate by comma then
  * space between the number must be printed
  * in assending orderonly putchar function is a
  * llowed putchar use at a amaximum time through,
- *  char type not allowd
+ * char type not allowd
  * Retrun: 0
  */
  void print_combine_(int n) 
