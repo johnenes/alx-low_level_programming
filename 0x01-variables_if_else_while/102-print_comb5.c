ab78cccb12ad70a2601cee5d9c3aa695143d7caf
johnenes <johneneojo1@gmail.com>
2023-06-17 01:53:06 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,35 +8,38 @@
  * print only the smallest combination of 3 digits.
  * Numbers should be printed in ascending order.
  * You can only use `putchar` to print to console.
- * You can only use `putchar` up to 6 times.
- * You are not allowed to use any variables of type `char`.
  * Return: 0
  */
 int main(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i <= 99; i++)
-    {
-        for (j = i; j <= 99; j++)
-        {
-            int tens1 = i / 10;
-            int ones1 = i % 10;
-            int tens2 = j / 10;
-            int ones2 = j % 10;
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i; j <= 99; j++)
+		{
+			int tens1 = i / 10;
+			int ones1 = i % 10;
+			int tens2 = j / 10;
+			int ones2 = j % 10;
 
-            putchar((tens1 / 10) + '0');
-            putchar((tens1 % 10) + '0');
-            putchar(ones1 + '0');
-            putchar(' ');
+			/* Print tens digit of the first number */
+			putchar(tens1 + '0');
+			/* Print ones digit of the first number */
+			putchar(ones1 + '0');
+			/* Print a space */
+			putchar(' ');
 
-            putchar((tens2 / 10) + '0');
-            putchar((tens2 % 10) + '0');
-            putchar(ones2 + '0');
-            putchar(',');
-            putchar(' ');
-        }
-    }
+			/* Print tens digit of the second number */
+			putchar(tens2 + '0');
+			/* Print ones digit of the second number */
+			putchar(ones2 + '0');
+			/* Print a comma */
+			putchar(',');
+			/* Print a space */
+			putchar(' ');
+		}
+	}
 
-    return 0;
+	return (0);
 }
