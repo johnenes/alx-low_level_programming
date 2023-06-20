8d4d8564a4009136f8abee817212f96f445d49a4
johnenes <johneneojo1@gmail.com>
2023-06-20 13:09:29 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,32 +1,19 @@
 #include<unistd.h>
 #include "main.h"
 /**
- * main - Entry Point
+ * print_alphabet - Entry Point
  * description: prints the alphabet, in lowercase
- * Return: 0
  */
-
-
-void print_alphabet(void);
-
-
-int main(void)
-{
-print_alphabet();
-return (0);
-}
-
-/**
- * print_alphabet - user defined function
- * description: this function process all the letters
-*/
 
 void print_alphabet(void)
 {
-char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-{
-_putchar(letter);
-_putchar('\n');
-}
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        write(1, &letter, 1);
+        letter++;
+    }
+
+    write(1, "\n", 1);
 }
