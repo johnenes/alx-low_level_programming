3058abd9d65fe5b6e2c61f1246af2e0cf9634993
johnenes <johneneojo1@gmail.com>
2023-06-20 05:50:58 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include<unistd.h>
+
+/* _putchar is a function take an argument*/
+/* char c is paramenter*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
+/* print_string_putchar  - is a function declaration*/
+/* description - a function called*/
+void print_string_putchar(const char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+
+}
+
+}
