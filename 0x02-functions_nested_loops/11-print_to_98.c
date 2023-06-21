3d0cd0ceb590e95e602632993561260e167c11b2
johnenes <johneneojo1@gmail.com>
2023-06-20 19:43:54 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,30 +1,48 @@
 #include "main.h"
 /**
+ * print_number - Prints a number
+ * @n: The number to be printed
+ */
+void print_number(int n)
+{
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
+}
+
+/**
  * print_to_98 - Prints natural numbers from n to 98
  * @n: The starting number
  */
-
- void print_to_98(int n);
 void print_to_98(int n)
 {
-if (n <= 98)
-{
-for (; n <= 98; n++)
-{
-_putchar("%d", n);
-if (n != 98)
-_putchar(", ");
-}
-}
-else
-{
-for (; n >= 98; n--)
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
 		{
-_putchar("%d", n);
-if (n != 98)
-_putchar(", ");
-}
-}
+			print_number(n);
 
-_putchar("\n");
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			print_number(n);
+
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+
+	_putchar('\n');
 }
