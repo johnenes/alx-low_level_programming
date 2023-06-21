0c6124c0cc38372980bff8b996e4d0ca9160009c
johnenes <johneneojo1@gmail.com>
2023-06-21 04:41:49 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,20 +7,19 @@
  */
 int main(void)
 {
-    unsigned long int prev = 1, curr = 2, sum = 0, temp;
+unsigned long int prev = 1, curr = 2, sum = 0, temp;
 
-    while (curr <= 4000000)
-    {
-        if (curr % 2 == 0)
-            sum += curr;
+while (curr <= 4000000)
+{
+if (curr % 2 == 0)
+sum += curr;
 
-        temp = curr;
-        curr += prev;
-        prev = temp;
-    }
-
-    printf("%lu\n", sum);
-
-    return 0;
+temp = curr;
+curr += prev;
+prev = temp;
 }
 
+printf("%lu\n", sum);
+
+return 0;
+}
