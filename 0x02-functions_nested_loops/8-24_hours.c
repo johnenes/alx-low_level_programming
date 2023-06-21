056d61b9ec1ecc96c5160c304e549a6b6ed58a28
johnenes <johneneojo1@gmail.com>
2023-06-20 18:41:19 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,14 +4,12 @@
  */
 
 void jack_bauer(void);
+
 void jack_bauer(void)
 {
 int hours, minutes;
-
 for (hours = 0; hours < 24; hours++)
-{
-		
-	
+{	
 for (minutes = 0; minutes < 60; minutes++)
 {
 _putchar((hours / 10) + '0');
