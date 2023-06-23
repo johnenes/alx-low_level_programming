8e5250ee9580f97691bf7df1188e71f027fdda6f
johnenes <johneneojo1@gmail.com>
2023-06-23 02:38:50 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,23 @@
-#include "main.h"
 int _putchar(char c);
+/*
+ * File: 4-print_most_numbers.c
+ * Auth: Suara Ayomide
+ */
+
+#include "main.h"
 
 /**
- * print_most_numbers - Prints numbers from 0 to 9 (excluding 2 and 4) followed by a new line
+ * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
  */
 void print_most_numbers(void)
 {
-	char digit;
+	int num;
 
-	for (digit = '0'; digit <= '9'; digit++)
+	for (num = 0; num <= 9; num++)
 	{
-		if (digit != '2' && digit != '4')
-			_putchar(digit);
+		if (num != 2 && num != 4)
+			_putchar((num % 10) + '0');
 	}
 
 	_putchar('\n');
 }
-
