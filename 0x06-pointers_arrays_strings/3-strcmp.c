86420500a2b45de59e24570d7d7c8e2393bf58c9
johnenes <johneneojo1@gmail.com>
2023-06-29 04:47:53 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,8 @@
  * @s1: The first string to compare.
  * @s2: The second string to compare.
  *
- * Return: Negative value if s1 is less than s2, positive value if s1 is greater than s2, 0 if they are equal.
+ * Return: Negative value if s1 is less than s2, positive value
+ * if s1 is greater than s2, 0 if they are equal.
  */
 int _strcmp(const char *s1, const char *s2)
 {
@@ -19,5 +20,5 @@ int _strcmp(const char *s1, const char *s2)
 		s2++;
 	}
 
-	return 0;
+	return (0);
 }
