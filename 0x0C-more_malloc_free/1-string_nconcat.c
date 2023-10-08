7a6d6fa824355edf7fc08b102968951366a76b5f
johnenes <johneneojo1@gmail.com>
2023-10-07 23:25:45 -0700

    1-string_nconcat edited

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,49 +8,43 @@
  * @s2: String 2
  * @n: number of byte to concatenate from s2
  * Return: concatenating String
+ *
+ * if malloc fails, status vslue is to 
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
-
 {
-unsigned int i;
-unsigned int s1len = 0;
-unsigned int s2len = 0;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
+	if (s1 == NULL)
+		s1 = "";
 
-char *output;
+	if (s2 == NULL)
+		s2 = "";
 
-if (s1 == NULL)
-s1 = "";
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
 
-if (s2 == NULL)
-s2 = "";
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
 
-for (i = 0; s1[i] != '\0'; i++)
-s1len++;
+	if (n > ls2)
+		n = ls2;
 
-for (i = 0; s2[i] != '\0'; i++)
-s2len++;
+	lsout = ls1 + n;
 
+	sout = malloc(lsout + 1);
 
-output = malloc(sizeof(char) * (s1len + 1) + 1);
-if (output == NULL)
-return (NULL);
-if (n >= s2len)
-{
-for (i = 0; s1[1] != '\0'; i++)
-output[i] = s1[i];
+	if (sout == NULL)
+		return (NULL);
 
-for (i = 0; s2[i] != '\0'; i++)
-output[s1len + i] = s2[i];
-output[s1len + i] = '\0';
-}
-else
-{
-for (i = 0; s1[i] != '\0'; i++)
-output[i] = s1[i];
-for (i = 0; i < n; i++)
-output[s1len + i] = s2[i];
-output[s1len + i] = '\0'; }
-return (output);
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
+
+	sout[i] = '\0';
+
+	return (sout);
 }
