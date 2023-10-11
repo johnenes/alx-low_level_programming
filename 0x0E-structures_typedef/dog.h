78936853efd354de0582145b4870076e333a6e52
johnenes <johneneojo1@gmail.com>
2023-10-10 21:51:59 -0700

    dog.h function

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+
+/**
+ * struct dog -  -do structure
+ * @name: name of the dog
+ * @age: dog age
+ * @owner: name of the dog owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
