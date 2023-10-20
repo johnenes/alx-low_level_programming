529bc73e042881291d817ef5cad06739ddd008cf
johnenes <johneneojo1@gmail.com>
2023-10-20 14:19:30 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,12 +1,17 @@
 #ifndef HEADER
 #define HEADER
 #include <stdio.h>
+#include <stddef.h>
+
+
+
 typedef struct list_s {
     char *str;
     int len;
     struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 
 
