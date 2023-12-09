09afe849615c123b44e307fcfb1aa9ed29c3ff2f
johnenes <johneneojo1@gmail.com>
2023-12-08 17:30:52 -0800

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,35 +1,32 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - Adds a new node at the end of a dlistint_t list
- * @head: Pointer to the pointer of the head of the list
- * @n: Value to be added to the new node
+ * get_dnodeint_at_index - returns the
+ * nth node of a dlistint_t linked list
  *
- * Return: Address of the new element, or NULL if it failed
+ * @head: head of the list
+ * @index: index of the nth node
+ * Return: nth node
  */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return NULL;
+	unsigned int i;
 
-    new_node->n = n;
-    new_node->next = NULL;
+	if (head == NULL)
+		return (NULL);
 
-    if (*head == NULL)
-    {
-        new_node->prev = NULL;
-        *head = new_node;
-        return new_node;
-    }
+	while (head->prev != NULL)
+		head = head->prev;
 
-    dlistint_t *last = *head;
-    while (last->next != NULL)
-        last = last->next;
+	i = 0;
 
-    last->next = new_node;
-    new_node->prev = last;
+	while (head != NULL)
+	{
+		if (i == index)
+			break;
+		head = head->next;
+		i++;
+	}
 
-    return new_node;
+	return (head);
 }
-
