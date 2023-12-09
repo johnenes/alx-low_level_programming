d319ccd1d19a9fdbe6f7b15356491da378b66c65
johnenes <johneneojo1@gmail.com>
2023-12-08 17:41:28 -0800

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef HEADER_H
 #define HEADER_H
 #include <stdlib.h>
+
 #include <stddef.h>
 #include<stdio.h>
 
@@ -29,7 +30,7 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
-
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 
 #endif
